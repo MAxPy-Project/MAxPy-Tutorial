ff2fce42de0e7665bbcf24bab8f0724d950e30d9
Yuri Arbeletche <ysba000@gmail.com>
2023-04-17 20:51:22 -0300

    added run2

--- testbench.py
+++ testbench.py
@@ -1,31 +1,40 @@
 import importlib
 from MAxPy import results
+from sklearn.metrics import mean_absolute_percentage_error
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import accuracy_score
 
 def testbench_run(ckt=None, results_filename=None):
     lin = ckt.poly1()
-    rst = results.ResultsTable(results_filename, ["mre"])
+    rst = results.ResultsTable(results_filename, ["mape", "mae", "accuracy"])
     print(f">>> testbench init - circuit: {lin.name()}, area: {lin.area}, parameters: {lin.parameters}")
-    mre = 0.0
-    a = 10
-    b = 20
-    lin.set_a(a)
-    lin.set_b(b)
-    for x in range(-128, 128):
-        lin.set_x(x)
-        lin.eval()
-        y_out = lin.get_y()
-        if y_out & 0x8000:
-            y_out ^= 0xffff
-            y_out += 1
-            y_out *= -1
-        y_ref = int(a*x) + b
-        if y_out != y_ref:
-            print(f"ref {y_ref}, out {y_out}")
+    y_true = []
+    y_pred = []
+    for a in range(-128, 128):
+        lin.set_a(a)
+        for b in range(-128, 128):
+            lin.set_b(b)
+            for x in range(-128, 128):
+                lin.set_x(x)
+                lin.eval()
+                y_out = lin.get_y()
+                if y_out & 0x8000:
+                    y_out ^= 0xffff
+                    y_out += 1
+                    y_out *= -1
+                y_ref = int(a*x) + b
+                if y_ref != 0:
+                    y_true.append(y_ref)
+                    y_pred.append(y_out)
+                count += 1
 
-    rst.add(lin, {"mre": mre})
-    print(f"> mre: {mre:.4f}")
+    mape = mean_absolute_percentage_error(y_true, y_pred)
+    mae = mean_absolute_error(y_true, y_pred)
+    accuracy = accuracy_score(y_true, y_pred)
+    rst.add(lin, {"mape": mape, "mae": mae, "accuracy": accuracy})
+    print(f"> mape: {mape:.4f}, mae: {mae:.4f}, accuracy: {accuracy:.4f}")
     print(">>> testbench end")
-    if mre < 0.1:
+    if mape < 0.1:
         prun_flag = True
     else:
         prun_flag = False
